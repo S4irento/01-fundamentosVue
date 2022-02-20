747c688cf19809520c093fc910da4436f758b2ee
Alvaro J Vanegas <alvarovanegasv28@gmail.com>
2022-02-20 00:21:34 -0500

    First steps

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CounterVue :start="10"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounterVue from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CounterVue
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <h3>{{ defaultTitle }}</h3>
+  <p>{{ counter }}</p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      "title": String,
+      "start": {
+        type: Number,
+        default: 0
+      }
+    },
+    data(){
+      return {
+        counter: this.start
+      }
+    },
+    methods: {
+      /** @method: incremente el valor en 1 */
+      increase(){
+        this.counter++;
+      },
+      /** @method: decrementa el valor en 1 */
+      decrease(){
+        this.counter--;
+      }
+    },
+    computed: {
+      defaultTitle(){
+        return this.title || 'Counter';
+      }
+    }
+
+  }
+</script>
+
+<style>
+  button{
+    background-color: rgb(14, 206, 78);
+    border: 1px solid white;
+    border-radius: 5px;
+    color: rgb(18, 32, 51);
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+  }
+
+  button:hover{
+    background-color: rgb(9, 139, 52);
+  }
+</style>
